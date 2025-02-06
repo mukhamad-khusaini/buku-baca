724260ba9bcac24eaf0286befbbea71a2509a733
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-02-06 20:58:14 +0700

    Add book and category factory then seed it

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3), // Judul dengan 3 kata
+            'author' => $this->faker->name,
+            'price' => $this->faker->randomFloat(2, 10, 100), // Harga antara 10 hingga 100
+            'image_url' => 'img/profile.jpeg',
+        ];
+    }
+}

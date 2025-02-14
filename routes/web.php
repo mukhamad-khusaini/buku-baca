f9d14c1c15dc2fe318d6349ffda3f1817cbd7ffc
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2025-02-14 10:22:50 +0700

    FIx auth controller

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
